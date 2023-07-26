d8d18d5987d22757915525502791ed07d2f03f6b
Brenn23 <140605974+Brenn23@users.noreply.github.com>
2023-07-26 11:33:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("service_s1acber", "template_o81k9fh", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
